f47d00baf046c1e1006bbe10d8a86268bc9e8cf6
Steven Harradine <stevenharradine@gmail.com>
2017-01-12 21:54:32 -0500

    update usage readme, read data from challage page

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # nationstates-challenge-aggregator
 Scans for all Nation States and then pulls each ones challenge stats writing them to a csv
+
+## Requirements
+ * node
+
+## Usage
+
+### install dependancies
+```
+npm install
+```
+
+### start scraping data
+```
+node scrape
+```
+
+### open in your favorite spread sheet app
+```
+libreoffice nations.csv
+```
--- scrape.js
+++ scrape.js
@@ -6,7 +6,7 @@ function scrape_page (page_start) {
 	var options = {
 		host: 'www.nationstates.net',
 		port: 443,
-		path: '/page=list_nations?censusid=254&amp;start=' + page_start
+		path: '/page=challenge/ladder=1/start='+ page_start
 	};
 
 	var req = https.request(options, function(res) {
@@ -19,24 +19,52 @@ function scrape_page (page_start) {
 		res.on('end', function () {
 			$ = cheerio.load(body);
 
-			links = $("a.nlink");
+			rows = $("tr");
 
-			links.each (function (index) {
-				var link_exploded = links[index].attribs.href.split("=");
-				link_exploded.shift();	// remove the key (key=value)
-				var nation_name = link_exploded.join("=")
+			rows.each (function (index) {
+				var row = cheerio.load (rows[index])("td");
+				if (index != 0) {
+					var nation = ""
+					var level = ""
+					var points = ""
+					var specialty = ""
+					var wins = -1
+					var losses = -1
+					var winrate = -1
 
-				console.log (nation_name);
+					row.each (function (index) {
+						var data = cheerio.load (row[index])("td").html()
 
-				fs.appendFile('message.txt', nation_name + "\n", function (err) {
+						switch (index) {
+							case 1: nation = data
+									break
+							case 2: level = data
+									break
+							case 3: points = data
+									break
+							case 4: specialty = data
+									break
+							case 5: wins = data
+									break
+							case 6: losses = data
+									break
+							case 7: winrate = data
+									break
+						}
+					});
 
-				});
+					var line_item = "\"" + nation + "\",\"" + level + "\",\"" + points + "\",\"" + specialty + "\",\"" + wins + "\",\"" + losses + "\",\"" + winrate + "\""
+					console.log ((page_start + index) + " " + nation);
+					fs.appendFile('nations.csv', line_item + "\n", function (err) {
+
+					});
+				}
 			})
 
-			if (page_start < 100) {
+			if (page_start < 164240) {
 				setTimeout(function () {
-					scrape_page (page_start + 10);
-				}, 2000);
+					scrape_page (page_start + 20);
+				}, 1000);
 			}
 		})
 	});
